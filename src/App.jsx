282faf8ead94f14e1ff940c0765984028e62a6ad
My application <ranasankit46@gmail.com>
2024-07-29 18:15:58 +0530

    Tech Stack added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ const App = () => {
   return (
     <div>
       <Header />
-      <Main/>
+      <Main />
+      
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import TechStack from './TechStack'
 
 const Main = () => {
   return (
-    <div className='flex items-center w-full p-10 justify-between'>
-      <div className='flex flex-col gap-4'>
-        <p className="text-6xl font-semibold text-wrap">Hi, I am Sankit Rana 👋</p>
-        <p className='text-xl'>- Frontend developer</p>
+    <div className='flex flex-col items-center w-full justify-between'>
+      <div className='flex w-full p-10'>
+        <div className='flex flex-col gap-4'>
+          <p className="text-6xl font-semibold text-wrap">Hi, I am Sankit Rana 👋</p>
+          <p className='text-xl'>- Frontend developer</p>
+        </div>
+        <div className='p-24 bg-slate-500 rounded'>
+          <p>Profiles</p>
+          <p>Linkedin - <span></span></p>
+          <p>Github</p>
+          <p>Email - <span></span></p>
+          <p></p>
+        </div>
       </div>
-      <div className='p-24 bg-slate-500 rounded'>
-        <p>Profiles</p>
-        <p>Linkedin - <span></span></p>
-        <p>Github</p>
-        <p>Email - <span></span></p>
-        <p></p>
-      </div>
+
+
+      <TechStack/>
 
     </div>
   )
